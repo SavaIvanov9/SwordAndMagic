761669fdcfc880591f1712cd9328069dc7e3c11a
monitobeko <monitobeko@abv.bg>
2015-12-15 18:47:35 +0200

    added base structure

---
+++ SwordAndMagic/Characters/PlayerControlled/Rogue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwordAndMagic.Characters.PlayerControlled
+{
+    public class Rogue
+    {
+    }
+}
---
+++ SwordAndMagic/Player/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SwordAndMagic.Player
+{
+    using Characters;
+
+    public abstract class Player : Character
+    {
+    }
+}
